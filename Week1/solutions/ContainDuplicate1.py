697d5f3c907032533c2c18df2940c1d519f7fd2c
1YileChen <yl2015000102@gmail.com>
2020-10-04 15:32:04 -0500

    add solutions for Contain Duplicates

---
+++ Week1/solutions/ContainDuplicate1.py
@@ -0,0 +1,7 @@
+#linear search
+for i in range(len(nums)):
+    for j in range(i+1, len(nums)):
+        if nums[i]==nums[j]:
+            return True
+return False
+        
---
+++ Week1/solutions/ContainDuplicate2.py
@@ -0,0 +1,8 @@
+#sorting
+nums.sort()
+for i in range(len(nums)-1):
+    if nums[i]==nums[i+1]:
+        return True
+return False
+
+        
